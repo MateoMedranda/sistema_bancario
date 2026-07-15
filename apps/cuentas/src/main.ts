b1bad585e2abd36900d7d1a8a10888c2d249f759
Tenkenoz <epobando1@espe.edu.ec>
2026-07-14 21:48:40 -0500

    Revert "fix: corrige import de Logger en microservicios TCP y Redis"
    This reverts commit b9ad5eaef4630f540c0e881cc837f3871af84d71.

--- apps/cuentas/src/main.ts
+++ apps/cuentas/src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { Transport, MicroserviceOptions } from '@nestjs/microservices';
-import { Logger } from '@nestjs/common';
+import { Transport, MicroserviceOptions, Logger } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
