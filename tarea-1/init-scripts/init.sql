7cbc3bc609b0a62ea6823fe7120c6491c543a8fc
Mateo Medranda <matemedranda15@gmail.com>
2026-07-16 23:33:05 -0500

    add(docker-compose.transportes.yml): Implementación de comunicación gRCP en el monorepo

--- tarea-1/init-scripts/init.sql
+++ tarea-1/init-scripts/init.sql
@@ -1,2 +1,18 @@
 CREATE DATABASE "TransaccionesBDD";
 CREATE DATABASE "CuentasBDD";
+
+\c "CuentasBDD"
+
+CREATE TABLE IF NOT EXISTS cuenta (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    "userId" VARCHAR(200) NOT NULL,
+    "accountNumber" VARCHAR(10) NOT NULL UNIQUE,
+    type VARCHAR(200) NOT NULL UNIQUE,
+    balance INTEGER NOT NULL,
+    status VARCHAR(20) NOT NULL,
+    "createdAt" TIMESTAMP NOT NULL DEFAULT now()
+);
+
+INSERT INTO cuenta (id, "userId", "accountNumber", type, balance, status) 
+VALUES ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'user1', '1234567890', 'AHORROS', 1000, 'ACTIVE')
+ON CONFLICT (id) DO NOTHING;
