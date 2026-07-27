727103a352529b2fcffd3828670a2335541092f9
Moises Benlacazar <benalcazarsebas8173@proton.me>
2026-07-26 22:11:31 -0500

    feat(observability): agregar integracion de Sentry y filtro global de excepciones

--- apps/sistema_bancario/src/main.ts
+++ apps/sistema_bancario/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe, Logger } from '@nestjs/common';
+import * as Sentry from '@sentry/nestjs';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './filters/all-exceptions.filter';
 
 async function bootstrap() {
+  // Inicializamos Sentry antes de crear la app NestJS
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN || 'https://839472395ffb94c66b3af18ee6de3e1d@o4511580718759936.ingest.us.sentry.io/4511804580298752',
+    tracesSampleRate: 1.0,
+  });
+
   const logger = new Logger('Gateway');
   const app = await NestFactory.create(AppModule);
 
